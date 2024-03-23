532651808dd8c7bb02e9143c0ed975db53344f27
RTY QWE <nurmatovnurbek555@gmail.com>
2024-03-23 09:44:40 +0500

    Добавьте файлы проекта.

---
+++ Project3/main.cpp
@@ -0,0 +1,60 @@
+#include <iostream>
+using namespace std;
+
+int main() 
+{
+    //Задание 1
+    
+    setlocale(LC_ALL, "ru");
+    /*
+    int n;
+    cout << "Введите число: ";
+    cin >> n;
+
+        int arr[3][3];
+
+    for (int i = 0; i < 3; i++)
+    {
+        if (i == 0) 
+        {
+            arr[0][i] = n;
+        }
+        else 
+        {
+            arr[0][i] = arr[0][i - 1] * 2;
+        }
+    }
+
+    for (int i = 0; i < 3; i++) 
+    {
+        cout << arr[0][i] << " ";
+    }
+    */
+    //Задание 2 
+    /*
+    int n;
+    cout << "Введите число: ";
+    cin >> n;
+    int arr[3][3];
+
+    for (int i = 0; i < 3; i++) 
+    {
+        if (i == 0) 
+        {
+            arr[0][i] = n;
+        }
+        else 
+        {
+            arr[0][i] = arr[0][i - 1] + 1;
+        }
+    }
+
+    for (int i = 0; i < 3; i++) 
+    {
+        cout << arr[0][i] << " ";
+    }
+    */
+
+
+    return 0;
+}
